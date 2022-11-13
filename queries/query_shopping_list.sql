3e9e6019f0597be23d5986734cf09839542b04e6
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-11-13 17:10:30 -0500

    Update query_shopping_list.sql

--- queries/query_shopping_list.sql
+++ queries/query_shopping_list.sql
@@ -7,11 +7,10 @@ SELEcT ingredient.name, ingredient.store_location
 	JOIN recipes AS recipe ON recipe.id = mr_map.recipe_id
 	JOIN recipe_ingredient_map AS map ON map.recipe_id = mr_map.recipe_id
 	JOIN ingredients AS ingredient ON ingredient.id = map.ingredient_id
-	WHERE meal.name='mexican dinner'
 UNION DISTINCT
-SELECT ingredient.name,  ingredient.store_location
-    FROM meal_ingredient_map AS map 
+SELECT ingredient.name, ingredient.store_location
+	FROM meal_ingredient_map AS map 
 	JOIN meals AS meal ON meal.id = map.meal_id
 	JOIN ingredients AS ingredient ON ingredient.id = map.ingredient_id
-	WHERE meal.name='mexican dinner'
+WHERE meal.name='mexican dinner'
 ORDER BY store_location;
