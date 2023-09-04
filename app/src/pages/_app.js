9c3c181fe7974577426432f41efaefb4424673b2
sw_ga_suki_sugi <timothy_shee@hotmail.com>
2023-09-04 16:55:27 -0400

    wip(recipe): Initialize RecipePage

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import theme from "@/utils/theme";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
