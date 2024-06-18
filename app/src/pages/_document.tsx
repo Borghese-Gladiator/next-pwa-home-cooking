875586f58bdd89b051b56c20d7dad8c6ef811760
neko_ninja <timothy_shee@hotmail.com>
2024-06-18 02:29:07 -0400

    fix(_document): vercel build error

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -6,8 +6,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" />
       </Head>
       <body>
         <Main />
