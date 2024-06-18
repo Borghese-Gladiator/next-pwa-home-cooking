9f5fd9a5176469b4b657910bcaede84c07c18e25
neko_ninja <timothy_shee@hotmail.com>
2024-06-18 02:51:34 -0400

    feat: NextNProgress

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps } from 'next/app'
 import React from "react";
+
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import NextNProgress from 'nextjs-progressbar';
+
 import theme from "@/src/utils/theme";
+
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -10,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <NextNProgress />
       <Component {...pageProps} />
     </ThemeProvider>
   );
